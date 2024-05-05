9bb0e619aff14d9c57085ecb42faa109759ba4e1
reviwh <reviwh2@gmail.com>
2024-05-05 20:11:32 +0700

    fix: getting all image from directory

--- sejarah/gambar.php
+++ sejarah/gambar.php
@@ -2,27 +2,27 @@
 
 header("Access-Control-Allow-Origin: *");
 
-include 'koneksi.php';
-
-$sql = "SELECT foto FROM sejarawan";
-$result = $koneksi->query($sql);
-
 $response = array();
+$dir = './gambar_berita';
+$accepted_format = array("jpg", "jpeg", "png", "gif");
 
-if ($result->num_rows > 0) {
-    $response['isSuccess'] = true;
-    $response['message'] = "Berhasil Menampilkan Data Gambar Berita";
-    $response['data'] = array();
-    
-    while ($row = $result->fetch_assoc()) {
-        $response['data'][] = $row['foto'];
-    }
-} else {
+if (!file_exists($dir)) {
     $response['isSuccess'] = false;
-    $response['message'] = "Gagal menampilkan Data Gambar Berita";
+    $response['message'] = "Gagal menemukan direktori gambar berita";
     $response['data'] = null;
+} else {
+    $filelist = scandir('./gambar_berita');
+    $response['isSuccess'] = true;
+    $response['message'] = "Berhasil menemukan gambar berita";
+
+    foreach ($filelist as $file) {
+        $file_parts = explode('.', $file);
+        $file_type = strtolower(end($file_parts));
+
+        if ($file !== '.' && $file !== '..' && in_array($file_type, $accepted_format)) {
+            $response['data'][] = $file;
+        }
+    };
 }
 
 echo json_encode($response);
-
-?>

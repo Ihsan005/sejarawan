9507174474a43fdf9d7cf147a118e40173076ada
reviwh <reviwh2@gmail.com>
2024-05-05 23:14:12 +0700

    feature: deleting foto sejarawan

--- sejarah/deletesejarawan.php
+++ sejarah/deletesejarawan.php
@@ -8,17 +8,29 @@ $id = $_POST['id'];
 // Buat query SQL untuk menghapus data berdasarkan ID
 $sql = "DELETE FROM sejarawan WHERE id = $id";
 
-// Jalankan query
-if ($koneksi->query($sql) === TRUE) {
-    // Jika penghapusan berhasil
-    $response['isSuccess'] = true;
-    $response['message'] = "Data sejarawan berhasil dihapus";
+$image = $koneksi->query("SELECT foto FROM sejarawan WHERE id = $id")->fetch_column();
+
+if ($image) {
+    $image_path = './gambar_berita/' . $image;
+
+    if (unlink($image_path)) {
+        // Jalankan query
+        if ($koneksi->query($sql) === TRUE) {
+            // Jika penghapusan berhasil
+            $response['isSuccess'] = true;
+            $response['message'] = "Data sejarawan berhasil dihapus";
+        } else {
+            // Jika terjadi kesalahan saat menghapus data
+            $response['isSuccess'] = false;
+            $response['message'] = 'Error: ' . $koneksi->error;
+        }
+    } else {
+        $response['isSuccess'] = false;
+        $response['message'] = "Gagal Menghapus Data";
+    }
 } else {
-    // Jika terjadi kesalahan saat menghapus data
     $response['isSuccess'] = false;
-    $response['message'] = 'Error: ' . $koneksi->error;
+    $response['message'] = "Gagal menemukan sejarawan";
 }
-
-
 // Mengirimkan response dalam format JSON
 echo json_encode($response);

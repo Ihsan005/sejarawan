571cbaf7fb53fcc5ceb4a553d933cdd9bcadf2c9
reviwh <reviwh2@gmail.com>
2024-05-05 22:34:10 +0700

    fix: remove unecessery response[data]

--- sejarah/updateuser.php
+++ sejarah/updateuser.php
@@ -3,13 +3,13 @@
 header("Access-Control-Allow-Origin: *");
 include 'koneksi.php';
 
-if($_SERVER['REQUEST_METHOD'] == "POST") {
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $response = array();
-    
+
     $id = $_POST['id'];
     $fullname = $_POST['fullname'];
-    $username = $_POST['username']; 
-    $nohp = $_POST['nohp']; 
+    $username = $_POST['username'];
+    $nohp = $_POST['nohp'];
     $nim = $_POST['nim'];
     $email = $_POST['email'];
     $alamat = $_POST['alamat'];
@@ -19,13 +19,6 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
     if ($koneksi->query($sql)) {
         $response['isSuccess'] = true;
         $response['message'] = "User Berhasil di Edit";
-        $response['fullname'] = $result['fullname'];
-        $response['username'] = $result['username'];
-        $response['nohp'] = $result['nohp']; 
-        $response['nim'] = $result['nim']; 
-        $response['email'] = $result['email']; 
-        $response['alamat'] = $result['alamat']; 
-        $response['id_user'] = $result['id_user'];
     } else {
         $response['isSuccess'] = false;
         $response['message'] = "Gagal Edit User";

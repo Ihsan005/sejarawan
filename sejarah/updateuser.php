8774457885c4e362887292edb009a51de80044f7
reviwh <reviwh2@gmail.com>
2024-05-05 19:40:15 +0700

    fix: removing unnecessary response[value]

--- sejarah/updateuser.php
+++ sejarah/updateuser.php
@@ -9,37 +9,27 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
     $id = $_POST['id'];
     $fullname = $_POST['fullname'];
     $username = $_POST['username']; 
-   // $password = $_POST['password']; 
     $nohp = $_POST['nohp']; 
     $nim = $_POST['nim'];
     $email = $_POST['email'];
     $alamat = $_POST['alamat'];
 
+    $sql = "UPDATE user SET fullname = '$fullname', username = '$username',  nohp = '$nohp', nim = '$nim', email = '$email', alamat = '$alamat', updated = NOW() WHERE id_user = $id";
 
-    $sql = "UPDATE user SET fullname = '$fullname', username = '$username',  nohp = '$nohp', nim = '$nim', email = '$email', alamat = '$alamat' WHERE id_user = $id";
-    $isSuccess = $koneksi->query($sql);
-
-    if ($isSuccess) {
-        $cek = "SELECT * FROM user WHERE id_user = $id";
-        $result = mysqli_fetch_array(mysqli_query($koneksi, $cek));
-        $response['is_success'] = true;
-        $response['value'] = 1;
+    if ($koneksi->query($sql)) {
+        $response['isSuccess'] = true;
         $response['message'] = "User Berhasil di Edit";
         $response['fullname'] = $result['fullname'];
         $response['username'] = $result['username'];
-        //$response['password'] = $result['password']; 
         $response['nohp'] = $result['nohp']; 
         $response['nim'] = $result['nim']; 
         $response['email'] = $result['email']; 
         $response['alamat'] = $result['alamat']; 
         $response['id_user'] = $result['id_user'];
     } else {
-        $response['is_success'] = false;
-        $response['value'] = 0;
+        $response['isSuccess'] = false;
         $response['message'] = "Gagal Edit User";
     }
 
     echo json_encode($response);
 }
-
-?>

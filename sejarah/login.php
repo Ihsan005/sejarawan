0b8272eb1b92b420c9896c0bf451b5e278be7437
reviwh <reviwh2@gmail.com>
2024-05-05 19:37:22 +0700

    fix response data

--- sejarah/login.php
+++ sejarah/login.php
@@ -5,7 +5,7 @@ header("Access-Control-Allow-Origin: *");
 
 include 'koneksi.php';
 
-if($_SERVER['REQUEST_METHOD'] == "POST") {
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
     $response = array();
     $username = $_POST['username'];
     $password = md5($_POST['password']);
@@ -13,22 +13,14 @@ if($_SERVER['REQUEST_METHOD'] == "POST") {
     $cek = "SELECT * FROM user WHERE username = '$username' AND password = '$password'";
     $result = mysqli_fetch_array(mysqli_query($koneksi, $cek));
 
-    if(isset($result)){
-        $response['value'] = 1;
+    if (isset($result)) {
+        $response['isSuccess'] = true;
         $response['message'] = "berhasil login";
-        $response['username'] = $result['username'];
-        $response['fullname'] = $result['fullname'];
-        $response['nim'] = $result['nim'];
-        $response['nohp'] = $result['nohp'];
-        $response['email'] = $result['email'];
-        $response['alamat'] = $result['alamat'];
-        $response['id_user'] = $result['id_user'];
+        $response['data'] = array($result);
         echo json_encode($response);
     } else {
-        $response['value'] = 0;
+        $response['isSuccess'] = false;
         $response['message'] = "Gagal login";
         echo json_encode($response);
     }
 }
-
-?>

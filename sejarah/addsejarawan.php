609cc427464bdb1318b4721bd63c305c85ee2383
reviwh <reviwh2@gmail.com>
2024-05-05 20:41:26 +0700

    feature: upload image

--- sejarah/addsejarawan.php
+++ sejarah/addsejarawan.php
@@ -5,21 +5,35 @@ header("Access-Control-Allow-Origin: *");
 include 'koneksi.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // Pastikan bahwa semua parameter yang diperlukan tersedia
-    if (isset($_POST['nama']) && isset($_POST['asal'])) {
+    if (
+        isset($_POST['nama'])
+        && isset($_POST['tgl_lahir'])
+        && isset($_POST['asal'])
+        && isset($_FILES['foto'])
+        && isset($_POST['tgl_lahir'])
+        && isset($_POST['jenis_kelamin'])
+        && isset($_POST['deskripsi'])
+    ) {
         $nama = $_POST['nama'];
         $tgl_lahir = $_POST['tgl_lahir'];
         $asal = $_POST['asal'];
+        $foto = $_FILES['foto'];
         $jenis_kelamin = $_POST['jenis_kelamin'];
         $deskripsi = $_POST['deskripsi'];
 
-        $sql = "INSERT INTO sejarawan (nama, tgl_lahir, asal, jenis_kelamin, deskripsi) VALUES ('$nama','$tgl_lahir', '$asal', '$jenis_kelamin','$deskripsi')";
-        if ($koneksi->query($sql) === TRUE) {
-            $response['isSuccess'] = true;
-            $response['message'] = "Berhasil menambahkan data sejarawan";
+        if (move_uploaded_file($foto['tmp_name'], "./gambar_berita/" . $foto['name'])) {
+            $query = $koneksi->prepare("INSERT INTO sejarawan (nama, tgl_lahir, asal, foto, jenis_kelamin, deskripsi) VALUES (?, ?, ?, ?, ?, ?)");
+            $query->bind_param("ssssss", $nama, $tgl_lahir, $asal, $foto['name'], $jenis_kelamin, $deskripsi);
+            if ($query->execute()) {
+                $response['isSuccess'] = true;
+                $response['message'] = "Berhasil menambahkan data sejarawan";
+            } else {
+                $response['isSuccess'] = false;
+                $response['message'] = "Gagal menambahkan data sejarawan: " . $koneksi->error;
+            }
         } else {
             $response['isSuccess'] = false;
-            $response['message'] = "Gagal menambahkan data sejarawan: " . $koneksi->error;
+            $response['message'] = "Gagal upload gambar";
         }
     } else {
         $response['isSuccess'] = false;
@@ -31,4 +45,3 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 
 echo json_encode($response);
-?>
